56bd2e05fddfcf3fef19a91562f49d5581843f91
Miguel <miguelashdez@gmail.com>
2021-02-24 13:59:40 -0600

    test git

---
+++ fundamentos/assets/js/bases/alerts.js
@@ -0,0 +1,11 @@
+// alert('Hola Mundo!');
+
+// let nombre = prompt('¿Cuál es tu nombre?');
+// console.log(nombre);
+// console.log('*****' + nombre + '*****');
+
+const seleccion = confirm('¿Está seguro de borrar esto?');
+console.log(seleccion);
+
+// El objeto global sólo existe en node
+// consolelo.log(global);
---
+++ fundamentos/assets/js/bases/objeto-literal.js
@@ -0,0 +1,58 @@
+const personaje = {
+    nombre: 'Tony Stark',
+    codeName: 'Ironman',
+    vivo: false,
+    edad: 40,
+    coords: {
+        lat: 34.034,
+        lng: -118.70
+    },
+    trajes: ['Mark I', 'Mark V', 'Hulkbuster'],
+    direccion: {
+        zip: '10880, 90265',
+        ubicacion: 'Malibu, California',
+    },
+    'ultima-pelicula': 'Infinity War'
+};
+
+console.log(personaje);
+console.log('Nombre', personaje.nombre);
+console.log('Nombre', personaje['nombre']);
+console.log('Edad', personaje.edad);
+
+console.log('Coords', personaje.coords);
+console.log('Lat', personaje.coords.lat);
+
+console.log('No. Trajes', personaje.trajes.length);
+console.log('último traje', personaje.trajes[personaje.trajes.length - 1]);
+
+const x = 'vivo';
+console.log('Vivo', personaje[x]);
+
+console.log('Última película', personaje['ultima-pelicula']);
+
+// Más detalles
+
+delete personaje.edad;
+console.log(personaje);
+
+personaje.casado = true;
+
+const entriesPares = Object.entries(personaje);
+console.log(entriesPares);
+
+// personaje = true;
+
+// Con el método freeze evitamos que se puedan cambiar las propiedades dentro de personaje
+Object.freeze(personaje);
+// Con el método freeze evitamos que se puedan cambiar las propiedades dentro de personaje.direccion
+Object.freeze(personaje.direccion);
+
+personaje.dinero = 10000000000;
+personaje.casado = false;
+personaje.direccion.ubicacion = 'Costa Rica';
+console.log(personaje);
+
+const propiedades = Object.getOwnPropertyNames(personaje);
+const valores = Object.values(personaje);
+console.log({ propiedades, valores });
